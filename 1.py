64b0337ebce3364a2e186981c38d21c4abc6011c
JTL66 <65905010+JTL66@users.noreply.github.com>
2020-08-28 22:22:07 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+Last login: Fri Aug 28 21:45:12 on ttys000
+(base) Galaxy-Tab-A-8:~ jtliu$ print"Hello World"
+-bash: printHello World: command not found
+(base) Galaxy-Tab-A-8:~ jtliu$ python
+Python 3.8.3 (default, Jul  2 2020, 11:26:31) 
+[Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print"Hello World"
+  File "<stdin>", line 1
+    print"Hello World"
+         ^
+SyntaxError: invalid syntax
+>>> print("Hello World")
+Hello World
+>>> 
+
+
+
+
+
+
+
+
+
