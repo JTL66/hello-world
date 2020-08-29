b655313fcbc789841a7e1dfa9349ceb6a4ecf61a
JTL66 <65905010+JTL66@users.noreply.github.com>
2020-08-28 22:26:57 -0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,24 @@
+Last login: Fri Aug 28 22:24:42 on ttys000
+(base) Galaxy-Tab-A-8:~ jtliu$ python
+Python 3.8.3 (default, Jul  2 2020, 11:26:31) 
+[Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello World")
+Hello World
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
